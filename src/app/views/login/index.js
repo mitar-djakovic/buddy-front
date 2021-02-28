a5751fb8160099658eda5a3454067e434a24ee0c
Mitar Djakovic <roker.93@hotmail.com>
2021-02-28 19:45:45 +0100

    Styled login page

--- src/app/views/login/index.js
+++ src/app/views/login/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { useStyles } from './style';
+import dog from '../../assets/dog.jpg';
+import logo from '../../assets/logo.png';
 
-const Login = () => <div>Login</div>;
+const Login = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.formContainer}>
+        <div>
+          <img src={logo} alt="" />
+        </div>
+      </div>
+      <div className={classes.infoContainer}>
+        <img className={classes.img} src={dog} alt="" />
+        <div className={classes.info}>
+          <h1 className={classes.adopt}>Adopt.</h1>
+          <h2 className={classes.dontShop}>Dont Shop.</h2>
+          <div className={classes.underline} />
+          <p className={classes.underlineText}>Find a best friend.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
---
+++ src/app/views/login/style.js
@@ -0,0 +1,49 @@
+import { createUseStyles } from 'react-jss';
+
+export const useStyles = createUseStyles({
+  wrapper: {
+    backgroundColor: '#ede8e2',
+    width: '100%',
+    height: '100vh',
+    display: 'flex',
+    overflow: 'hidden',
+  },
+  infoContainer: {
+    position: 'relative',
+    width: '50%',
+    boxSizing: 'border-box',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  info: {
+    zIndex: 2,
+    position: 'absolute',
+    bottom: 140,
+  },
+  img: {
+    width: '100%',
+    zIndex: 1,
+  },
+  adopt: {
+    color: '#2f3543',
+    fontSize: 80,
+  },
+  dontShop: {
+    color: '#ff6b81',
+    fontSize: 80,
+  },
+  underline: {
+    width: '100%',
+    height: 4,
+    backgroundColor: '#2f3543',
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  underlineText: {
+    fontSize: 30,
+    color: '#2f3543',
+  },
+  formContainer: {
+    width: '50%',
+  },
+});
